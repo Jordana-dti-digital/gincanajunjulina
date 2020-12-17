5c2ed81db93633ddad8d35e27a3dde40c2dc52be
Joao Pedro Renan <jota.renan@gmail.com>
2020-12-17 19:48:25 -0300

    Insere cartões de verdade

--- src/data/cards.ts
+++ src/data/cards.ts
@@ -1,13 +1,16 @@
 import { CardInfo } from "../types";
 
 const Cards: CardInfo[] = [
-    { titulo: 'Cartao 1', urlPreview: 'print.png', url: '#' },
-    { titulo: 'Cartao 2', urlPreview: 'print.png', url: '#' },
-    { titulo: 'Cartao 3', urlPreview: 'print.png', url: '#' },
-    { titulo: 'Cartao 4', urlPreview: 'print.png', url: '#' },
-    { titulo: 'Cartao 5', urlPreview: 'print.png', url: '#' },
-    { titulo: 'Cartao 6', urlPreview: 'print.png', url: '#' },
-    { titulo: 'Cartao 7', urlPreview: 'print.png', url: '#' },
+    { titulo: 'Mensagem da Rena', url: 'https://elastic-colden-929f7a.netlify.app/', urlPreview: './previews/1.png' },
+    { titulo: 'Globo de Neve: a magia do natal', url: 'https://maripaf.github.io/christmas-card/', urlPreview: './previews/2.png' },
+    { titulo: 'Stranger Natal', url: 'https://cartao-natal-stranger-things.vercel.app/', urlPreview: './previews/3.png' },
+    { titulo: 'Christmas Card - HoHoHo', url: 'https://brunocesarr.github.io/christmas-card/', urlPreview: './previews/4.png' },
+    { titulo: 'Brasilian Natataletion', url: 'https://nataldti.github.io/', urlPreview: './previews/5.png' },
+    { titulo: 'Natal na Praça Sete', url: 'https://natal-praca-sete.netlify.app/', urlPreview: './previews/6.png' },
+    { titulo: 'Então é Natal', url: 'https://entao-eh-natal.netlify.app/', urlPreview: './previews/7.png' },
+    { titulo: 'Christmas Cats', url: 'https://guimma.github.io/Christmas-Cats/', urlPreview: './previews/8.png' },
+    { titulo: 'A Saga de Noel', url: 'https://5fda973577a1f931bb780d52--unruffled-swanson-e5cd45.netlify.app/', urlPreview: './previews/9.png' },
+
 ];
 
 export default Cards;
