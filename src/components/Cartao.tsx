7dccf0c396d283fc2c91efcff80c968ad2ac7170
Jordana-dti-digital <86578659+Jordana-dti-digital@users.noreply.github.com>
2021-07-04 17:36:01 -0300

    Update Cartao.tsx
    Alterações para gincana

--- src/components/Cartao.tsx
+++ src/components/Cartao.tsx
@@ -1,20 +1,14 @@
 import Lottie, { Options } from 'react-lottie';
-import BonitoData from '../assets/lottie-bonito.json';
 import EngracadoData from '../assets/lottie-engracado.json';
 import { CardInfo } from '../types';
-
-
 
 interface Props {
     config: CardInfo,
     setFunniest: (config: any) => void;
-    setMostBeautiful: (config: any) => void;
     isFunniest: boolean;
-    isMostBeautiful: boolean;
 };
 
 export default function Cartao({ config, setFunniest, setMostBeautiful, isFunniest, isMostBeautiful }: Props) {
-    const beautifulLottieOptions = { animationData: BonitoData, autoplay: false, loop: false } as Options;
     const funnyLottieOptions = { animationData: EngracadoData, autoplay: false, loop: false } as Options;
     const { urlPreview, titulo, url } = config;
     return (
@@ -25,21 +19,8 @@ export default function Cartao({ config, setFunniest, setMostBeautiful, isFunnie
                     <img src={urlPreview} alt={titulo} />
                 </div>
             </a>
-            <section className="voting">
-                <button className={`register-vote-button ${isMostBeautiful ? 'active' : ''}`} onClick={() => {
-                    if (isMostBeautiful) {
-                        setMostBeautiful(undefined);
-                    } else {
-                        setMostBeautiful(config);
-                    }
-                }
-                }>
-                    <Lottie options={beautifulLottieOptions}
-                        style={{ margin: 0 }}
-                        width="32px"
-                        height="32px"></Lottie>
-                    <span>{isMostBeautiful ? "Desmarcar +Bonito" : "Marcar +Bonito"}</span>
-                </button>
+            <section className="voting">               
+                    
                 <button className={`register-vote-button ${isFunniest ? 'active' : ''}`} onClick={() => {
                     if (isFunniest) {
                         setFunniest(undefined);
